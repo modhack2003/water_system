b3953860962381465c6131427ac2e9c753f59431
bikram@dey2003 <62688713+modhack2003@users.noreply.github.com>
2024-10-06 08:31:19 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import MoistureDisplay from './MoistureDisplay';
+
+function App() {
+  return (
+    <div>
+      <MoistureDisplay />
+    </div>
+  );
+}
+
+export default App;
